9bb16f7598bac5ae5eb443646c95ecc445edba3b
Lucas Cardoso de Menezês <lucascmenezes00@gmail.com>
2024-03-24 17:12:53 -0300

    Subindo os primeiro códigos

--- Haarcascade Framework/ScriptVideo.py
+++ Haarcascade Framework/ScriptVideo.py
@@ -1,7 +1,8 @@
 import cv2 #Importanto o pacote cv2 para reconhecimento do imagens
 
 webcam = cv2.VideoCapture(1) #Definindo uma variável que irá receber a caputra da minha webcam
-classificadorVideoFace  = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')  #Importando o XML de reconhecimento de face
+classificadorVideoFace = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')  #Importando o XML de reconhecimento de face
+classificadorVideoOlho = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
 
 #looping que irá realizar a leitura da face aparecendo na webcam e colcoar um retangulo na face detectada.
 
@@ -15,6 +16,12 @@ while True:
 
     for(x, y, l, a) in detecta:
         cv2.rectangle(frame, (x, y), (x + l, y + a), (255, 0, 0), 2)
+
+        olho = frame[y:y + a, x:x + l]
+        olhoCinza = cv2.cvtColor(olho, cv2.COLOR_BGR2GRAY)
+        detectaOlho = classificadorVideoOlho.detectMultiScale(olhoCinza)
+        for (ox, oy, ol, oa) in detectaOlho:
+            cv2.rectangle(olho, (ox, oy), (ox + ol, oy + oa), (0, 255, 0), 2)
 
 
     cv2.imshow('Imagem WebCamera', frame) #Exibindo o resultado
